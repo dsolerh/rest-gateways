40c4d1474ec9fd6c9e52f060cffb57fce3111bb6
dsolerh <dsolerh.cinter95@gmail.com>
2022-02-28 20:55:46 -0500

    (add test) create test for delete feature

---
+++ tests/e2e/gateway/delete.spec.js
@@ -0,0 +1,40 @@
+const app = require("../../../src/server");
+const GatewayModel = require("../../../src/modules/gateway/models/gateway.model");
+const {
+  createConnection,
+  closeConnection,
+} = require("../../utils/setup-mongoose-conection");
+const supertest = require("supertest");
+
+beforeEach(createConnection);
+
+afterEach(closeConnection);
+
+describe("DELETE /api/gateway", () => {
+  test("(200 Ok) delete an existing gateway", async () => {
+    const data = {
+      serialNumber: "G212",
+      name: "Lorem ipsum",
+      IPV4Address: "123.12.3.1",
+    };
+    const g = await GatewayModel.create(data);
+
+    await supertest(app)
+      .delete(`/api/gateway/${g._id}`)
+      .expect(204)
+      .then(async () => {
+        const gateway = await GatewayModel.findById(g._id);
+        expect(gateway).toBeNull();
+      });
+  });
+
+  test("(400 Bad Request) invalid id for mongoose ObjectId", async () => {
+    await supertest(app).delete("/api/gateway/qwe121w21").expect(400);
+  });
+
+  test("(404 Not Found) cant found the gateway", async () => {
+    await supertest(app)
+      .delete("/api/gateway/ffffffffffffffffffffffff")
+      .expect(404);
+  });
+});
