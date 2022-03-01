217ddf97bddf2d82aa2ea9726b94825faa3a2c0d
dsolerh <dsolerh.cinter95@gmail.com>
2022-03-01 09:34:41 -0500

    (update)

--- src/modules/gateway/routes.config.js
+++ src/modules/gateway/routes.config.js
@@ -3,11 +3,14 @@ const GatewayController = require("./controllers/gateway.controller");
 
 console.log("Initializing routes: Gateway");
 
+// basic gateway CRUD
 router.post("/gateway", GatewayController.createGateway);
 router.get("/gateway", GatewayController.listGateways);
 router.get("/gateway/:id", GatewayController.getById);
 router.patch("/gateway/:id", GatewayController.updateGateway);
 router.delete("/gateway/:id", GatewayController.deleteGateway);
+
+// devices management
 router.post("/gateway/:id/add-device", GatewayController.addDevice);
 router.delete(
   "/gateway/:id/remove-device/:deviceId",
